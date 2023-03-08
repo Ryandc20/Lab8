3b3285016a9895e6233003702ddb27d90b3f1efa
Ryan Cooper <ryan.dm.cooper@gmail.com>
2023-03-07 22:26:13 -0700

    GREEN: passed count cities test

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -74,6 +74,6 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public int countCities() {
-        return -1;
+        return cities.size();
     }
 }
--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -53,6 +53,7 @@ public class CustomListTest {
 
         assertEquals(0, list.countCities());
         City city = new City("Edmonton", "Alberta");
+        list.addCity(city);
         assertEquals(1, list.countCities());
     }
 }
